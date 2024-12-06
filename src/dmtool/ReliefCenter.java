9869c0db91f292cc2843ffac78c4150b001ffdd3
AmilaPerera93 <adpamila@gmail.com>
2024-12-06 20:02:57 +0530

    Adding relief centers

---
+++ src/dmtool/ReliefCenter.java
@@ -0,0 +1,23 @@
+public class ReliefCenter {
+    private final String name;
+    private final double latitude;
+    private final double longitude;
+
+    public ReliefCenter(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+}
