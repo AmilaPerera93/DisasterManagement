f8c1fd4f97cfb21f6693fde5bfaace7dfa2b1ac6
AmilaPerera93 <adpamila@gmail.com>
2024-12-06 20:03:50 +0530

    Adding request locations

---
+++ src/dmtool/RequestLocation.java
@@ -0,0 +1,23 @@
+public class RequestLocation {
+    private final String name;
+    private final double latitude;
+    private final double longitude;
+
+    public RequestLocation(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+}
